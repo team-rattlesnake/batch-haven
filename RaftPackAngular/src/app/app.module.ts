35a672efc2eb6db5edb036f0ef7ed163c58a68e1
mnnnnn <mnguyen5081@gmail.com>
2018-02-21 03:39:10 -0500

    working profile component with Http

--- RaftPackAngular/src/app/app.module.ts
+++ RaftPackAngular/src/app/app.module.ts
@@ -1,18 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+// tslint:disable-next-line:import-spacing
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileDetailComponent } from './profile-detail/profile-detail.component';
+import { ProfileService } from './profile.service';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProfileDetailComponent,
+    MessagesComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false })
   ],
-  providers: [],
+  providers: [ProfileService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
