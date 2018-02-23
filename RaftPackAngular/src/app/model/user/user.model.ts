37347691797f571a39393cde4a41f6f770453722
Demarques Glass <cortez.glass@yahoo.com>
2018-02-23 14:51:47 -0800

    implementing register functionality

---
+++ RaftPackAngular/src/app/model/user/user.model.ts
@@ -0,0 +1,20 @@
+export class User {
+
+firstname: string;
+lastname: string;
+email: string;
+password: string;
+age: number;
+
+
+constructor(firstname: string, lastname: string, email: string, password: string, age: number){
+  this.firstname = firstname;
+  this.lastname = lastname;
+  this.email = email;
+  this.password = password;
+  this.age = age;
+}
+}
+
+
+
--- RaftPackAngular/src/app/register/register.component.ts
+++ RaftPackAngular/src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../model/user/user.model';
+import { RegisterService } from '../service/register.service';
+import { Message } from '../model/message.model';
+
 
 
 @Component({
@@ -7,10 +11,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  title = 'User Registration';
 
-  constructor() { }
+  constructor(private registerService: RegisterService ) { }
 
-  ngOnInit() {
-  }
+ // For data binding
+ public user: User = new User('', '', '', '', 0);
+
+ // To message the user
+ public message: Message = new Message('');
+
+ registerHero(): void {
+   this.registerService.registerUser(this.user).subscribe(
+     message => this.message = message,
+     error => this.message.text = 'Something went wrong.');
+ }
 
 }
