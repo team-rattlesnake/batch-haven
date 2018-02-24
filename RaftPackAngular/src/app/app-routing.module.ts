280023b24d3176e00a3142ea6c4af87d8b24f58c
DamonDantin <damondantin@gmail.com>
2018-02-23 19:18:27 -0500

    Uneeded- Commiting to be able to pull

--- RaftPackAngular/src/app/app-routing.module.ts
+++ RaftPackAngular/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
 import { ModifyUserInfoComponent } from './modify-user-info/modify-user-info.component';
+import { PasswordResetComponent } from './password-reset/password-reset.component'
 
 const routes: Routes = [
- { path: 'modify-user-info',component : ModifyUserInfoComponent}
+ { path: 'modify-user-info',component : ModifyUserInfoComponent},
+ { path: 'password-reset', component : PasswordResetComponent}
 ]
 
 
--- RaftPackAngular/src/app/app.module.ts
+++ RaftPackAngular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule }   from '@angular/forms';//practice
 import { AppComponent } from './app.component';
 import { ModifyUserInfoComponent } from './modify-user-info/modify-user-info.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PasswordResetComponent } from './password-reset/password-reset.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ModifyUserInfoComponent
+    ModifyUserInfoComponent,
+    PasswordResetComponent
   ],
   imports: [
     BrowserModule,
