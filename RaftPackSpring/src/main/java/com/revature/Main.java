4d133653ed92e7e8548add90622a862145157580
Demarques Glass <cortez.glass@yahoo.com>
2018-03-01 19:43:51 -0800

    changes

--- RaftPackSpring/src/main/java/com/revature/Main.java
+++ RaftPackSpring/src/main/java/com/revature/Main.java
@@ -1,15 +1,23 @@
 package com.revature;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Component;
+
 import com.revature.model.User;
 import com.revature.repository.UserRepository;
 import com.revature.repository.UserRepositoryImpl;
 import com.revature.service.UserServiceImpl;
 
 public class Main {
-static UserRepository up  = new UserRepositoryImpl();
+	
+	public static ApplicationContext appContext= new ClassPathXmlApplicationContext("applicationContext.xml");
+
 	public static void main(String[] args) {
 		
-		UserServiceImpl usl = new UserServiceImpl(up);
+		
+		UserServiceImpl usl = appContext.getBean("userService", UserServiceImpl.class);
 		
 		User me  = new User("Demarques", "Glass", "cortez.glass@yahoo.com" , "123", "male", "1994-10-19");		
 
